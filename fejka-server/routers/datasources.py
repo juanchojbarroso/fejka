e33ce9ded5505abc9cfeae378d4a09723e7ccd20
Juan Barroso <juanchojbarroso@gmail.com>
2022-05-22 18:18:06 +0100

    Get a datasource

--- fejka-server/routers/datasources.py
+++ fejka-server/routers/datasources.py
@@ -11,3 +11,9 @@ async def datasources():
     print(f"Get all datasource")
     datasouces = getDatasources()
     return datasouces
+
+@router.get("/datasources/{datasource_id}")
+def get_datasource(datasource_id):
+    print(f"Get datasource with id {int(datasource_id)}")
+    datasouces = getDatasources()
+    return datasouces[int(datasource_id)]
