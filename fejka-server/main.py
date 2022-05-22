8bb55cbda2568adca8d9ccc0e7b7defd5ccb328b
Juan Barroso <juanchojbarroso@gmail.com>
2022-05-22 18:21:01 +0100

    Remove origin restrinctions

--- fejka-server/main.py
+++ fejka-server/main.py
@@ -7,13 +7,8 @@ from internal import admin
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
