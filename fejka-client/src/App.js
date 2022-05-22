7c65cafb94fd864ce0374211a0a2d362be3c1118
Juan Barroso <juanchojbarroso@gmail.com>
2022-05-22 21:48:16 +0100

    Add barchart

--- fejka-client/src/App.js
+++ fejka-client/src/App.js
@@ -1,12 +1,47 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Selector from "./components/Selector";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Container from '@mui/material/Container';
 import "./App.css";
+
 import MainDashboard from "./screens/main-dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MainDashboard />
-      </header>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            Fejka
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="xl">
+        <Stack spacing={2}>
+          <Grid container spacing={2}>
+            <Grid
+              item
+              xs={8}
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Selector />
+            </Grid>
+          </Grid>
+          <MainDashboard />
+        </Stack>
+      </Container>
     </div>
   );
 }
--- fejka-client/src/screens/main-dashboard.js
+++ fejka-client/src/screens/main-dashboard.js
@@ -1,28 +1,10 @@
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import BarChart from "../components/BarChart";
 
 function MainDashboard(params) {
   return (
-    <Box ml={{}}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <Box>
+      <BarChart />
     </Box>
   );
 }
