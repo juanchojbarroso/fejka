408390038d5452f29a8ebb0c66b8e25d60fd95bc
Juan Barroso <juanchojbarroso@gmail.com>
2022-05-22 18:18:45 +0100

    Get dataset for a datasource

--- fejka-server/routers/datasources.py
+++ fejka-server/routers/datasources.py
@@ -17,3 +17,12 @@ def get_datasource(datasource_id):
     print(f"Get datasource with id {int(datasource_id)}")
     datasouces = getDatasources()
     return datasouces[int(datasource_id)]
+
+@router.get("/datasources/{datasource_id}/dataset")
+def get_dataset(datasource_id):
+    print(f"Get dataset for datasource with id {int(datasource_id)}")
+    datasouce = transformJsonToObject(getDatasources()[int(datasource_id)])
+    print(datasouce)
+
+    data = pd.read_csv(datasouce.url)
+    return Response(data.to_json(orient="records"), media_type="application/json")
