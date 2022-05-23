c25b4d2a3d6302c5954919403616ef266c149942
Juan Barroso <juanchojbarroso@gmail.com>
2022-05-23 19:13:36 +0100

    Allow localhost corn

--- fejka-server/main.py
+++ fejka-server/main.py
@@ -7,8 +7,15 @@ from internal import admin
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
