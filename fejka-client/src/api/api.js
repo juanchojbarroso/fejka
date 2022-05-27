d45585a6fe173ca8fa7df2bdbb98117b39c9a81b
Juan Barroso <juanchojbarroso@gmail.com>
2022-05-27 16:00:13 +0100

    Fetch data from server

--- fejka-client/src/api/api.js
+++ fejka-client/src/api/api.js
@@ -5,53 +5,20 @@ function fetchDatasources() {
   });
 }
 
+function fetchDatasourcesLabes({id}) {
+  return fetch(`${process.env.REACT_APP_API_URL}/datasources/${id}/dataset/labels`).then((res) => {
+    const result = res.json();
+    return result;
+  });
+}
 
-const labels = ["name", "uv", "pv", "amt"];
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+function fetchDatasourcesDataset({id}) {
+  return fetch(`${process.env.REACT_APP_API_URL}/datasources/${id}/dataset`).then((res) => {
+    const result = res.json();
+    return result;
+  });
+}
 
 
-export { fetchDatasources, data, labels};
+
+export { fetchDatasources, fetchDatasourcesDataset, fetchDatasourcesLabes};
