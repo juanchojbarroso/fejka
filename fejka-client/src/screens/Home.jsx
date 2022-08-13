1883b00e852b4e665cb7ce315b4ddcb4b0e3fa78
Juan Barroso <jbarroso@mymoid.com>
2022-08-13 10:58:29 +0100

    link to Chart from home

--- fejka-client/src/screens/Home.jsx
+++ fejka-client/src/screens/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link, Box, Flex, Text, Center, Icon } from "@chakra-ui/react";
+import { FcPieChart } from "react-icons/fc";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Flex alignItems="center">
+      <Box w="100%" h="100%">
+        <Link href={"/charts"}>
+          <Center>
+            <Icon as={FcPieChart} w={20} h={20} />
+            <Text
+              bgGradient="linear(to-l, #7928CA, #FF0080)"
+              bgClip="text"
+              fontSize="6xl"
+              fontWeight="extrabold"
+            >
+              Go to Charts
+            </Text>
+          </Center>
+        </Link>
+      </Box>
+    </Flex>
+  );
 }
