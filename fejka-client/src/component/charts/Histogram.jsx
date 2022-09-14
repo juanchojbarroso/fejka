0ef468ad06f088200c9a5a2f0b2796710fc2ec1c
Juan Barroso <jbarroso@mymoid.com>
2022-09-14 22:42:38 +0100

    Histograma

--- fejka-client/src/component/charts/Histogram.jsx
+++ fejka-client/src/component/charts/Histogram.jsx
@@ -1,51 +1,136 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { Chart } from "react-google-charts";
+import { useQuery } from "react-query";
+import { QueryCache } from "react-query";
+import Info from "../Info";
+import Error from "../Error";
+import Warning from "../Warning";
+import { useDataSetKeys } from "../../hooks/dataset";
+import { useDataSet } from "../../contexts/DataSets";
+import { useSelectedKeys } from "../../contexts/SelectedKeys";
+import AxisSelector from "../AxisSelector";
+import { fetchDatasourcesDataset } from "../../api";
+import {
+  getValueFromSelectedKeys,
+  transformDataToGoogleDataTable,
+} from "../../utils/value";
 
-export const data = [
-  ["Dinosaur", "Length"],
-  ["Acrocanthosaurus (top-spined lizard)", 12.2],
-  ["Albertosaurus (Alberta lizard)", 9.1],
-  ["Allosaurus (other lizard)", 12.2],
-  ["Apatosaurus (deceptive lizard)", 22.9],
-  ["Archaeopteryx (ancient wing)", 0.9],
-  ["Argentinosaurus (Argentina lizard)", 36.6],
-  ["Baryonyx (heavy claws)", 9.1],
-  ["Brachiosaurus (arm lizard)", 30.5],
-  ["Ceratosaurus (horned lizard)", 6.1],
-  ["Coelophysis (hollow form)", 2.7],
-  ["Compsognathus (elegant jaw)", 0.9],
-  ["Deinonychus (terrible claw)", 2.7],
-  ["Diplodocus (double beam)", 27.1],
-  ["Dromicelomimus (emu mimic)", 3.4],
-  ["Gallimimus (fowl mimic)", 5.5],
-  ["Mamenchisaurus (Mamenchi lizard)", 21.0],
-  ["Megalosaurus (big lizard)", 7.9],
-  ["Microvenator (small hunter)", 1.2],
-  ["Ornithomimus (bird mimic)", 4.6],
-  ["Oviraptor (egg robber)", 1.5],
-  ["Plateosaurus (flat lizard)", 7.9],
-  ["Sauronithoides (narrow-clawed lizard)", 2.0],
-  ["Seismosaurus (tremor lizard)", 45.7],
-  ["Spinosaurus (spiny lizard)", 12.2],
-  ["Supersaurus (super lizard)", 30.5],
-  ["Tyrannosaurus (tyrant lizard)", 15.2],
-  ["Ultrasaurus (ultra lizard)", 30.5],
-  ["Velociraptor (swift robber)", 1.8],
-];
-
-export const options = {
-  title: "Lengths of dinosaurs, in meters",
-  legend: { position: "none" },
-};
+const CHART_NAME = "HistogramChart";
+const queryCache = new QueryCache({
+  onError: (error) => {
+    console.log(error);
+  },
+  onSuccess: (data) => {
+    console.log(data);
+  },
+});
 
 export function HistogramChart() {
+  // GLOBAL
+  const { dataSet } = useDataSet();
+  const dataSetID = dataSet?.id;
+  const { data: keys } = useDataSetKeys();
+  const { selectedKeys, updateSelectedKeys } = useSelectedKeys();
+
+  // LOCAL
+  const [axisX, setAxisX] = useState(null);
+  const [axisY, setAxisY] = useState(null);
+  const columns = [axisX, axisY, ...getValueFromSelectedKeys(selectedKeys)];
+  const shouldfetchData = Boolean(axisX) && Boolean(axisY);
+
+  const { data, error, isError, isLoading, refetch } = useQuery(
+    [CHART_NAME],
+    () => fetchDatasourcesDataset(dataSetID, columns),
+    {
+      enabled: false,
+    }
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      refetch();
+    }
+    if (shouldfetchData) {
+      fetchData();
+    }
+  }, [axisX, axisY, selectedKeys]);
+
+  useEffect(() => {
+    setAxisX(null);
+    setAxisY(null);
+    updateSelectedKeys([]);
+    queryCache.clear();
+  }, [dataSet]);
+
+  const options = {
+    chart: {
+      title: `${dataSet?.name} / ${CHART_NAME}`,
+    },
+    legend: { position: "none" },
+  };
+
+  if (!Boolean(dataSet)) {
+    return (
+      <Warning
+        heading="Falta lo más importante!"
+        text="Tienes que seleccionar un dataset."
+      />
+    );
+  }
+  if (isLoading) {
+    return (
+      <Info
+        heading="Loading.."
+        text="Los perezosos pueden aguantar más tiempo el aliento que los delfines"
+      />
+    );
+  }
+  if (isError) {
+    return <Error heading="Uppss algo fue mal." text={error} />;
+  }
   return (
-    <Chart
-      chartType="Histogram"
-      width="100%"
-      height="400px"
-      data={data}
-      options={options}
-    />
+    <>
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <GridItem w="100%" h="10">
+          <AxisSelector
+            value={axisX}
+            placeholder="Axis X"
+            isDisabled={false}
+            onChange={(e) => {
+              console.log(e);
+              setAxisX(e.target.value);
+            }}
+            keys={keys}
+          />
+        </GridItem>
+        <GridItem w="100%" h="10">
+          <AxisSelector
+            value={axisY}
+            placeholder="Axis Y"
+            isDisabled={false}
+            onChange={(e) => {
+              console.log(e);
+              setAxisY(e.target.value);
+            }}
+            keys={keys}
+          />
+        </GridItem>
+      </Grid>
+      {!Boolean(data) ? (
+        <Info
+          heading="Falta una cosilla..."
+          text="Es necesario el valor de 'Axis X' y 'Axis Y' para ver el grafico"
+        />
+      ) : (
+        <Chart
+        chartType="Histogram"ƒ
+          width="100%"
+          height="400px"
+          data={transformDataToGoogleDataTable(data)}
+          options={options}
+        />
+      )}
+    </>
   );
 }
