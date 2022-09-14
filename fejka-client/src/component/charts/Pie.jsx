82bdbb2ac0703f20d407ebbe47bfbe8e42af5f9d
Juan Barroso <jbarroso@mymoid.com>
2022-09-14 23:15:41 +0100

    Simple pie

--- fejka-client/src/component/charts/Pie.jsx
+++ fejka-client/src/component/charts/Pie.jsx
@@ -1,27 +1,134 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { Chart } from "react-google-charts";
+import { useQuery } from "react-query";
+import { QueryCache } from "react-query";
+import Info from "../Info";
+import Error from "../Error";
+import Warning from "../Warning";
+import { useDataSetKeys } from "../../hooks/dataset";
+import { useDataSet } from "../../contexts/DataSets";
+import { useSelectedKeys } from "../../contexts/SelectedKeys";
+import AxisSelector from "../AxisSelector";
+import { fetchDatasourcesDataset } from "../../api";
+import {
+  transformDataToGoogleDataTable,
+} from "../../utils/value";
 
-const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 11],
-  ["Eat", 2],
-  ["Commute", 2],
-  ["Watch TV", 2],
-  ["Sleep", 7],
-];
-
-const options = {
-  title: "My Daily Activities",
-};
+const CHART_NAME = "PieChart";
+const queryCache = new QueryCache({
+  onError: (error) => {
+    console.log(error);
+  },
+  onSuccess: (data) => {
+    console.log(data);
+  },
+});
 
 export function SimplePieChart() {
+  // GLOBAL
+  const { dataSet } = useDataSet();
+  const dataSetID = dataSet?.id;
+  const { data: keys } = useDataSetKeys();
+  const { updateSelectedKeys } = useSelectedKeys();
+
+  // LOCAL
+  const [axisX, setAxisX] = useState(null);
+  const [axisY, setAxisY] = useState(null);
+  const columns = [axisX, axisY];
+  const shouldfetchData = Boolean(axisX) && Boolean(axisY);
+
+  const { data, error, isError, isLoading, refetch } = useQuery(
+    [CHART_NAME],
+    () => fetchDatasourcesDataset(dataSetID, columns),
+    {
+      enabled: false,
+    }
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      refetch();
+    }
+    if (shouldfetchData) {
+      fetchData();
+    }
+  }, [axisX, axisY]);
+
+  useEffect(() => {
+    setAxisX(null);
+    setAxisY(null);
+    updateSelectedKeys([]);
+    queryCache.clear();
+  }, [dataSet]);
+
+  const options = {
+    chart: {
+      title: `${dataSet?.name} / ${CHART_NAME}`
+    },
+  };
+
+  if (!Boolean(dataSet)) {
+    return (
+      <Warning
+        heading="Falta lo más importante!"
+        text="Tienes que seleccionar un dataset."
+      />
+    );
+  }
+  if (isLoading) {
+    return (
+      <Info
+        heading="Loading.."
+        text="Los perezosos pueden aguantar más tiempo el aliento que los delfines"
+      />
+    );
+  }
+  if (isError) {
+    return <Error heading="Uppss algo fue mal." text={error} />;
+  }
   return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"400px"}
-    />
+    <>
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        <GridItem w="100%" h="10">
+          <AxisSelector
+            value={axisX}
+            placeholder="Axis X"
+            isDisabled={false}
+            onChange={(e) => {
+              console.log(e);
+              setAxisX(e.target.value);
+            }}
+            keys={keys}
+          />
+        </GridItem>
+        <GridItem w="100%" h="10">
+          <AxisSelector
+            value={axisY}
+            placeholder="Axis Y"
+            isDisabled={false}
+            onChange={(e) => {
+              console.log(e);
+              setAxisY(e.target.value);
+            }}
+            keys={keys}
+          />
+        </GridItem>
+      </Grid>
+      {!Boolean(data) ? (
+        <Info
+          heading="Falta una cosilla..."
+          text="Es necesario el valor de 'Axis X' y 'Axis Y' para ver el grafico"
+        />
+      ) : (
+        <Chart
+          chartType={CHART_NAME}
+          width="100%"
+          height="400px"
+          data={transformDataToGoogleDataTable(data)}
+          options={options}
+        />
+      )}
+    </>
   );
 }
